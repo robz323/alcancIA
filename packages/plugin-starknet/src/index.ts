d7ee7b9c808f7cc40608ae97b45e43447579fa85
rodrigo ramos xochiteotzin <rodrx20@gmail.com>
2025-07-24 13:57:06 -0600

    feat: discord client removed

--- packages/plugin-starknet/src/index.ts
+++ packages/plugin-starknet/src/index.ts
@@ -3,9 +3,14 @@ import { executeSwap } from "./actions/swap";
 import transfer from "./actions/transfer";
 import { deployToken } from "./actions/unruggable";
 import transferSubdomain from "./actions/subdomain";
-import createWalletAction from "./actions/createWallet";
 import createInvisibleWalletAction from "./actions/createInvisibleWallet";
 import recoverWalletAction from "./actions/recoverWallet";
+import {
+  createPersonalSavingsGoalAction,
+  depositToPersonalSavingsAction,
+  createGroupSavingsAction,
+  depositToGroupSavingsAction
+} from "./actions/ahorro";
 
 export const PROVIDER_CONFIG = {
     AVNU_API: "https://starknet.impulse.avnu.fi/v1",
@@ -15,6 +20,7 @@ export const PROVIDER_CONFIG = {
         BTC: "0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac",
         ETH: "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
         STRK: "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
+        USDC: "0x53c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8",
     },
     TOKEN_SECURITY_ENDPOINT: "/defi/token_security?address=",
     TOKEN_TRADE_DATA_ENDPOINT: "/defi/v3/token/trade-data/single?address=",
@@ -26,13 +32,16 @@ export const starknetPlugin: Plugin = {
     name: "starknet",
     description: "Starknet Plugin for Eliza",
     actions: [
-        // createWalletAction, // Temporalmente desactivado por error en ChipiPay SDK
         createInvisibleWalletAction, 
         recoverWalletAction, 
         transfer, 
         executeSwap, 
         deployToken, 
-        transferSubdomain
+        transferSubdomain,
+        createPersonalSavingsGoalAction,
+        depositToPersonalSavingsAction,
+        createGroupSavingsAction,
+        depositToGroupSavingsAction
     ],
     evaluators: [],
     providers: [],
